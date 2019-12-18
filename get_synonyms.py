1f8fd8f28ca9bf744cdf1b146ef80715ff07deea
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-12-18 14:09:17 +0200

    Add --min-length argument

--- get_synonyms.py
+++ get_synonyms.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Helper for generate_synonyms.py. Given a TSV file with (protein-id,
+# interpro-id, sequence), groups sequences sharing the same
+# interpro-id and outputs a TSV file with (interpro-id, sequence-list)
+# where sequence-list is a space-separated list of sequences.
+
 import sys
 
 from collections import defaultdict, Counter
@@ -8,6 +13,8 @@ from collections import defaultdict, Counter
 def argparser():
     from argparse import ArgumentParser
     ap = ArgumentParser()
+    ap.add_argument('--min-length', default=2, type=int,
+                    help='Minimum length of sequences to include')
     ap.add_argument('--min-count', default=None, type=int,
                     help='Minimum occurrence number of sequences to include')
     ap.add_argument('file', nargs='+')
@@ -24,11 +31,14 @@ def get_synonyms(fn, sequence_count_by_id, args):
                                  ' on line {} in {}: {}'.format(
                                      len(fields), ln, fn, l))
             protein_id, interpro_id, sequence = fields
-            sequence_count_by_id[interpro_id][sequence] += 1
+            if len(sequence) >= args.min_length:
+                sequence_count_by_id[interpro_id][sequence] += 1
 
 
 def main(argv):
     args = argparser().parse_args(argv[1:])
+    if args.min_length < 2:
+        raise ValueError('--min-length must be 2 or greater')
     sequence_count_by_id = defaultdict(Counter)
     for fn in args.file:
         get_synonyms(fn, sequence_count_by_id, args)
